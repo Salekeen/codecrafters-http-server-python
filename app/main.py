f3bd91e5f15fe2ee1f995fc1070fa521c0f90dbc
salekeennayeem <nawaz.nayeem@selise.ch>
2025-01-01 20:50:37 +0600

    respond with 200

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    sock_obj, addr = server_socket.accept()  # wait for client
+    sock_obj.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
