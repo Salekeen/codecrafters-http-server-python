299c341181a8d02560edcef4d0a22a3f547d358b
salekeennayeem <nawaz.nayeem@selise.ch>
2025-01-02 23:30:49 +0600

    extracted url path

--- app/main.py
+++ app/main.py
@@ -5,8 +5,12 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    sock_obj, addr = server_socket.accept()  # wait for client
-    sock_obj.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    client, addr = server_socket.accept()  # wait for client
+    client_msg = client.recv(4096).decode().split(" ")
+    if client_msg[1] == "/":
+        client.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        client.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 
 if __name__ == "__main__":
